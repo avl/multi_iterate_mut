ca4702c04a4e45d26745315ae02f2797db6b1cfb
Anders Musikka <anders@andersmusikka.se>
2020-02-09 00:24:29 +0100

    Wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![feature(test)]
+#![deny(warnings)]
 
-use core::any::Any;
 use std::marker::PhantomData;
 use scoped_threadpool::Pool;
 
@@ -9,25 +9,27 @@ struct FuncThreadData<FA1> {
     data: Vec<Vec<(usize,FA1)>>
 }
 
-struct MultiIterateMut1<'a,'b,'c,T,X, A1, FA1:for <'r> FnOnce(&'r mut A1)> {
+pub struct MultiIterateMut1<'a,'b,'c,T,X:Default, A1, FA1:for <'r> FnOnce(&'r mut A1)> {
     data : &'a mut Vec<T>,
     attached: &'c mut Vec<X>,
     aux : (&'b mut Vec<A1>,),
+    #[allow(dead_code)]
     p1: PhantomData<FA1>,
 }
 
-struct MultiIterateMut2<'a,'b,'c,T,X,
+pub struct MultiIterateMut2<'a,'b,'c,T,X:Default,
     A1, FA1:for <'r> FnOnce(&'r mut A1),
     A2, FA2:for <'r> FnOnce(&'r mut A2)
 > {
     data : &'a mut Vec<T>,
     attached: &'c mut Vec<X>,
     aux : (&'b mut Vec<A1>,&'b mut Vec<A2>),
+    #[allow(dead_code)]
     p1: (PhantomData<FA1>,PhantomData<FA2>),
 }
 
 
-impl<'a,'b,'c,T,X,
+impl<'a,'b,'c,T,X:Default,
     A1, FA1:for <'r> FnOnce(&'r mut A1),
 > MultiIterateMut1<'a,'b,'c,T,X,A1,FA1> {
     pub fn new(data:&'a mut Vec<T>, attached: &'c mut Vec<X>, aux:(&'b mut Vec<A1>,)) -> MultiIterateMut1<'a,'b,'c,T,X,A1,FA1> {
@@ -39,7 +41,7 @@ impl<'a,'b,'c,T,X,
     }
 }
 
-impl<'a,'b,'c,T,X,
+impl<'a,'b,'c,T,X:Default,
     A1, FA1:for <'r> FnOnce(&'r mut A1),
     A2, FA2:for <'r> FnOnce(&'r mut A2)
 > MultiIterateMut2<'a,'b,'c, T,X,A1,FA1,A2,FA2> {
@@ -53,14 +55,14 @@ impl<'a,'b,'c,T,X,
 }
 
 
-struct MultiIterateContext1<'a,T, A1,FA1:for <'r> FnOnce(&'r mut A1)> {
+pub struct MultiIterateContext1<'a,T, A1,FA1:for <'r> FnOnce(&'r mut A1)> {
     chunk_size:usize,
     aux_mutator_closures:(&'a mut FuncThreadData<FA1>,),
     p1:PhantomData<T>,
     p2:PhantomData<A1>
 }
 
-struct MultiIterateContext2<'a,T,
+pub struct MultiIterateContext2<'a,T,
     A1,FA1:for <'r> FnOnce(&'r mut A1),
     A2,FA2:for <'r> FnOnce(&'r mut A2)
 > {
@@ -130,17 +132,20 @@ macro_rules! make_run1 {
 
                 let auxref = &self.aux;
                 let $aux_ref = &mut $aux_closures;
-
+                self.attached.resize_with(pool_size,||Default::default());
+                let self_attached = &mut self.attached;
                 let l_ref = &l;
                 pool.scoped(move |scope| {
-                    for (datachunk_items, aux_mutators) in &mut selfdata.chunks_mut(chunk_size)
+                    for ((datachunk_items, attached_item), aux_mutators) in &mut selfdata.chunks_mut(chunk_size)
+                        .zip(self_attached.iter_mut())
                         .zip(emit_aux_iter!($aux_ref, $($tuple_keys),+)) {
+
                         let mut context = $MultiIterateContext1::new(
                                 aux_mutators,chunk_size);
 
                         scope.execute(move || {
                             for item in datachunk_items {
-                                l_ref(item, &mut context, auxref);
+                                l_ref(item, &mut context, auxref, attached_item);
                             }
                         });
                     }
@@ -158,32 +163,32 @@ macro_rules! make_run1 {
     }
 }
 
-impl<'a,'b,'c,T:Send,X:Send,A1:Send+Sync,FA1:for <'r> FnOnce(&'r mut A1) + Send > MultiIterateMut1<'a,'b,'c,T,X,A1,FA1> {
+impl<'a,'b,'c,T:Send,X:Send+Default,A1:Send+Sync,FA1:for <'r> FnOnce(&'r mut A1) + Send > MultiIterateMut1<'a,'b,'c,T,X,A1,FA1> {
     make_run1!(run1,
-    (Fn(&mut T, &mut MultiIterateContext1<T,A1,FA1>, &(&mut Vec<A1>,))),
+    (Fn(&mut T, &mut MultiIterateContext1<T,A1,FA1>, &(&mut Vec<A1>,), &mut X)),
     aux_closures, MultiIterateContext1, aux_closures_ref, 0);
 }
 
-impl<'a,'b,'c,T:Send,X:Send,
+impl<'a,'b,'c,T:Send,X:Send+Default,
     A1:Send+Sync,FA1:for <'r> FnOnce(&'r mut A1) + Send,
     A2:Send+Sync,FA2:for <'r> FnOnce(&'r mut A2) + Send,
 > MultiIterateMut2<'a,'b,'c,T,X,A1,FA1,A2,FA2> {
     make_run1!(
         run1,
-        (Fn(&mut T, &mut MultiIterateContext2<T,A1,FA1,A2,FA2>, &(&mut Vec<A1>,&mut Vec<A2>))),
+        (Fn(&mut T, &mut MultiIterateContext2<T,A1,FA1,A2,FA2>, &(&mut Vec<A1>,&mut Vec<A2>), &mut X)),
         aux_closures, MultiIterateContext2, aux_closures_ref, 0, 1);
 }
 
 
 #[inline]
-fn run_aux<A1:Send,FA1:for <'r> FnOnce(&'r mut A1) + Send>(mut aux_and_closures: (&mut Vec<A1>, Vec<FuncThreadData<FA1>>) , aux_data: &mut AuxData) {
+fn run_aux<A1:Send,FA1:for <'r> FnOnce(&'r mut A1) + Send>(aux_and_closures: (&mut Vec<A1>, Vec<FuncThreadData<FA1>>) , aux_data: &mut AuxData) {
     let AuxData{pool, pool_size, chunk_size} = aux_data;
     let chunk_size = *chunk_size;
     let pool_size = *pool_size;
 
     let (aux1, mut aux1_closures) = aux_and_closures;
     let mut aux1_pass_slices = Vec::new();
-    for i in 0..pool_size {
+    for _ in 0..pool_size {
         aux1_pass_slices.push(Vec::new());
     }
     for aux1_thread_output in aux1_closures.iter_mut() {
@@ -234,7 +239,7 @@ impl<'a,T,A1,FA1:for <'r> FnOnce(&'r mut A1)> MultiIterateContext1<'a,T,A1,FA1> 
         }
     }
     #[inline(always)]
-    fn modify_aux(&mut self, aux_index: usize, f:FA1) {
+    pub fn modify_aux(&mut self, aux_index: usize, f:FA1) {
         let pass = aux_index / self.chunk_size;
         (self.aux_mutator_closures.0.data)[pass].push((aux_index, f));
     }
@@ -254,26 +259,28 @@ impl<'a,T,
         }
     }
     #[inline(always)]
-    fn modify_aux0(&mut self, aux_index: usize, f:FA1) {
+    pub fn modify_aux0(&mut self, aux_index: usize, f:FA1) {
         let pass = aux_index / self.chunk_size;
         (self.aux_mutator_closures.0.data)[pass].push((aux_index, f));
     }
     #[inline(always)]
-    fn modify_aux1(&mut self, aux_index: usize, f:FA2) {
+    pub fn modify_aux1(&mut self, aux_index: usize, f:FA2) {
         let pass = aux_index / self.chunk_size;
         (self.aux_mutator_closures.1.data)[pass].push((aux_index, f));
     }
 }
 
 
-pub struct UsizeExampleItem {
-    item : usize,
-}
 
 
 
 #[cfg(test)]
-mod tests {
+pub mod tests {
+    pub struct UsizeExampleItem {
+        item : usize,
+        #[allow(dead_code)]
+        payload: [u64;15],
+    }
     extern crate test;
     use super::MultiIterateMut1;
     use super::MultiIterateMut2;
@@ -294,9 +301,10 @@ mod tests {
         test.aux.0.push("Före".to_string());
 
         let mut pool = Pool::new(8);
-        test.run1(&mut pool, |data_item, test, aux1|{
+        test.run1(&mut pool, |data_item, test, _aux1, _|{
             *data_item=38;
             test.modify_aux(1,|arg:&mut String|{*arg = "Hej".to_string()});
+            //test.modify_aux(2,|arg:&mut String|{*arg = "Hej2".to_string()});
         });
 
         assert_eq!(test.data[0],38);
@@ -309,15 +317,17 @@ mod tests {
 
     }
 
+
     #[test]
     fn it_works2() {
 
         let mut data = Vec::new();
         let mut aux1 = Vec::new();
         let mut aux2 = Vec::new();
-        let mut t  = Vec::<()>::new();
-        let mut test = MultiIterateMut2::new(&mut data, &mut t,(&mut aux1, &mut aux2));
-        compile_error!("Use attached storage (per thread)")
+
+        let mut attached = Vec::new();
+        let mut test = MultiIterateMut2::new(&mut data, &mut attached,(&mut aux1, &mut aux2));
+
         test.data.push(37);
         test.aux.0.push("Före0".to_string());
         test.aux.0.push("Före0".to_string());
@@ -325,13 +335,15 @@ mod tests {
         test.aux.1.push("Före1".to_string());
 
         let mut pool = Pool::new(8);
-        test.run1(&mut pool, |data_item, test, aux1|{
+        test.run1(&mut pool, |data_item, test, _aux1,attached|{
             *data_item=38;
             test.modify_aux0(1,|arg:&mut String|{*arg = "Hej0".to_string()});
             test.modify_aux1(1,|arg:&mut String|{*arg = "Hej1".to_string()});
+            *attached = 411;
         });
 
         assert_eq!(test.data[0],38);
+        assert_eq!(test.attached[0],411);
         assert_eq!(test.aux.0[0],"Före0");
         assert_eq!(test.aux.0[1],"Hej0");
         assert_eq!(test.aux.1[0],"Före1");
@@ -340,11 +352,9 @@ mod tests {
         println!("Result: {:?}",test.aux.0);
 
         return ();
+    }
 
-    }
-    struct UsizeExampleItem {
-        item : usize,
-    }
+
     #[bench]
     fn benchmark_multi(bench:&mut Bencher) {
         let mut pool = Pool::new(8);
@@ -361,22 +371,24 @@ mod tests {
         for i in 0..data_size {
             test.data.push(UsizeExampleItem {
                 item:i,
+                payload: Default::default(),
             });
             test.aux.0.push(UsizeExampleItem {
                 item:1,
+                payload: Default::default(),
             });
         }
         bench.iter(move||{
 
 
-            test.run1(&mut pool, |data_item, test, aux1|{
+            test.run1(&mut pool, |data_item, test, _aux1,_|{
                 let primary_value= data_item.item;
 
                 {
                     test.modify_aux(primary_value,
                                     #[inline(always)]
                                         move|aux1:&mut UsizeExampleItem|{
-                                        aux1.item =((primary_value as f64*0.001).cos().cos().cos()*100.0) as usize;
+                                        aux1.item = primary_value; //((primary_value as f64*0.001).cos().cos().cos()*100.0) as usize;
                                     });
                 }
 
@@ -396,16 +408,18 @@ mod tests {
         for i in 0..data_size {
             data.push(UsizeExampleItem {
                 item:i,
+                payload: Default::default(),
             });
             aux1.push(UsizeExampleItem {
                 item:1,
+                payload: Default::default(),
             });
         }
         bench.iter(move||{
 
 
             for (aux1,data) in &mut aux1.iter_mut().zip(data.iter_mut()) {
-                aux1.item = ((data.item as f64*0.001).cos().cos().cos()*100.0) as usize;
+                aux1.item = data.item; //((data.item as f64*0.001).cos().cos().cos()*100.0) as usize;
             }
         });
     }
